390e3b59046c8b2cc92cf43c94a0cdc41d004a8d
Pavel K <pk@1702.ru>
2023-04-25 01:12:02 +0300

    Update 3_pipeline.py

--- 3_pipeline.py
+++ 3_pipeline.py
@@ -42,20 +42,29 @@ with open('ch_hostname.txt', 'r', encoding='utf-8') as fp:
 
 # определяем функцию, которая будет выполнять ETL
 def pipeline():
+    
     # получаем вчершнюю дату для sql-запроса и правильного именования CSV-файла, в который будем сохранять датафрейм
     yesterday = (date.today() - timedelta(days=1)).strftime('%Y-%m-%d')
+    
     # создаем подключение к БД PostgreSQL
     conn = create_engine(f'postgresql://{pg_user}:{pg_password}@{pg_hostname}/db_name')
+    
     # т.к. SELECT выполняем из специально подготовленного для этих целей VIEW, то не указываем столбцы поименно, чтобы не загромождать код
     sql_query = f'SELECT * FROM Lessons_View WHERE lesson_start_at = "{yesterday}";'
+    
     # сохраняем полученную выгрузку в датафрейм
     df = pd.read_sql_query(sql_query, conn)
+    
     # Рвем подключение к БД PostgreSQL
     conn.dispose()
     
-
-
-
+    # переопределяем типы данных по столбцам
+    df.astype({'lesson_id': 'uint64', 'lesson_title': 'str', 'lesson_description': 'str', 'lesson_start_at': 'datetime64',
+               'lesson_end_at': 'datetime64', 'lesson_homework_url': 'str', 'lesson_teacher': 'uint32', 'lesson_join_url': 'str',
+               'lesson_rec_url': 'str', 'module_id': 'uint32', 'module_title': 'str', 'module_description': 'str', 'module_created_at': 'datetime64',
+               'module_updated_at': 'datetime64', 'module_order_in_stream': 'uint8', 'stream_id': 'uint32', 'stream_name': 'str', 'stream_description': 'str',
+               'stream_start_at': 'datetime64', 'stream_end_at': 'datetime64', 'stream_created_at': 'datetime64', 'stream_updated_at': 'datetime64',
+               'course_id': 'uint16', 'course_title': 'str', 'course_description': 'str', 'course_created_at': 'datetime64', 'course_updated_at': 'datetime64'})
 
     # сохраняем датафрейм в CSV-файл
     df.to_csv(f'{yesterday}.csv', index=False)
@@ -71,7 +80,7 @@ def pipeline():
 
 # устанавливаем параметры DAG'a, время запуска в формате крона - ежедневно в 3 утра
 with DAG(
-    dag_id='incremental_daily_load', 
+    dag_id='daily_etl', 
     defaultargs=defaultargs, 
     catchup=False, 
     schedule_interval='0 3 * * *'
@@ -88,7 +97,7 @@ with DAG(
         task_id='send_email',
         to='airflow@sky.pro',
         subject='ClickHouse Daily Incremental Load Done',
-        html_content='<p>Ежедневная загрузка в ClickHouse выполнена.</p>',
+        html_content='<p>Ежедневная загрузка в ClickHouse выполнена.</p>'
     )
     
     # устанавливаем порядок выполнения task'ов
